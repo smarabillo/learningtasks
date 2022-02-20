35ed1b69a3d85ae30d1298dee9be9c7518b76546
smarabillo <100067653+smarabillo@users.noreply.github.com>
2022-02-20 21:40:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to My Website! I am Shemrei Marabillo!</h1>
     <ul>
        <li><a href="index.php">HOME</a></li>
-       <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+       <li><a href="/LE2/index.html">LEARNING TASK 2</a></li>
     </ul>
     </body>
 </html>
