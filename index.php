ebbd2cffb8f54c9e4596f61698998e016d74e0c0
smarabillo <100067653+smarabillo@users.noreply.github.com>
2022-02-20 17:07:34 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+    <head>  
+        <title>My Home Page</title>
+    </head>
+    <body>
+    <h1>Welcome to My Website! I am Shemrei Marabillo!</h1>
+    <ul>
+       <li><a href="index.php">HOME</a></li>
+    </ul>
+    </body>
+</html>
