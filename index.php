e6a20113db28c5710a2a4ff5b8bc8f53f1cef956
smarabillo <100067653+smarabillo@users.noreply.github.com>
2022-04-25 13:02:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
        <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
        <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
        <li><a href="/LT4/index.html">LEARNING TASK 4</a></li>
-        <li><a href="/CLE1MT/index.html">MIDTERM EXAM</a></li>
+       <li><a href="/LT5/index.html">LEARNING TASK 5</a></li>
+       <li><a href="/CLE1MT/index.html">MIDTERM EXAM</a></li>
     </ul>
     </body>
 </html>
