4440e779348d181654de64b4abb7d0cfd5ed74a2
smarabillo <100067653+smarabillo@users.noreply.github.com>
2022-05-21 21:53:03 +0800

    Add files via upload

---
+++ default.php
@@ -0,0 +1,19 @@
+<?php $page = (isset($_GET ['page']) && $_GET['page'] !='') ? $_GET['page'] : '';?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home Page</title>
+        <style>
+        <?php include 'CSS/style.css'; ?>
+        </style>
+        
+    </head>
+    <body>
+    <script src="JS/script.js"></script>
+        <div id="header">
+            <h1>Welcome <span>Home</span></h1>
+            <p>Hi I am Shemrei, check out my Website!</p>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,18 +1,42 @@
-<!DOCTYPE>
+<?php $page = (isset($_GET ['page']) && $_GET['page'] !='') ? $_GET['page'] : '';?>
+<!DOCTYPE html>
 <html>
-    <head>  
-        <title>My Home Page</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>SBM PORTFOLIO</title>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">  
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap" rel="stylesheet">
+        <link rel="stylesheet"  href="=style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap" rel="stylesheet">
+        <style>
+        <?php include 'CSS/style.css'; ?>
+        </style>
+        
     </head>
     <body>
-    <h1>Welcome to My Website! I am Shemrei Marabillo!</h1>
-    <ul>
-       <li><a href="index.php">HOME</a></li>
-       <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-       <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
-       <li><a href="/LT4/index.html">LEARNING TASK 4</a></li>
-       <li><a href="/LT5/index.html">LEARNING TASK 5</a></li>
-       <li><a href="/CLE1MT/index.html">MIDTERM EXAM</a></li>
-       <li><a href="/CLE1 (Endterm)/index.php">ENDTERM EXAM</a></li>
-    </ul>
+        <header>
+        <div id="nav"> 
+                <h1><span>SBM |</span>| Portfolio</h1>
+                <a href="index.php?page=about me">About Me</a>
+                <a href="index.php?page=my works">My Works</a>
+                <a href="index.php">Home</a>   
+                
+            <?php  
+                switch($page) {
+                case "my works":
+                include "my works.php";
+                break;
+                case "about me":
+                include "about me.php";
+               break;
+                default:
+                include "default.php";
+                break;
+            }
+            ?>            
+        </header>
     </body>
 </html>
