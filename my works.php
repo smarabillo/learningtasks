26e19adc036f644d962899dd5c263e5f2829878a
smarabillo <100067653+smarabillo@users.noreply.github.com>
2022-05-21 21:54:46 +0800

    Update my works.php

--- my works.php
+++ my works.php
@@ -11,11 +11,11 @@
     </head>
         <body>
         <div class="wrapper-wrk">
-            <a href="My works/CLE1MT/index.html" target="_blank">MIDTERM EXAM</a>
-            <a href="My Works/LT5/index.html" target="_blank">LEARNING TASK 5</a>
-            <a href="My Works/LT4/index.html" target="_blank">LEARNING TASK 4</a>
-            <a href="My Works/LT3/index.html" target="_blank">LEARNING TASK 3</a>
-            <a href="My Works/LT2/index.html" target="_blank">LEARNING TASK 2</a>
+            <a href="CLE1MT/index.html" target="_blank">MIDTERM EXAM</a>
+            <a href="LT5/index.html" target="_blank">LEARNING TASK 5</a>
+            <a href="LT4/index.html" target="_blank">LEARNING TASK 4</a>
+            <a href="LT3/index.html" target="_blank">LEARNING TASK 3</a>
+            <a href="LT2/index.html" target="_blank">LEARNING TASK 2</a>
         </div>
         <div class="wrapper-wrk-content">
             <p>Check out my works above!</p>
